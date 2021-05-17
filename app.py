9fd9b1a3332b56501caf4d2201e13cea93141467
dlkennen <dlkennen@comcast.net>
2021-05-17 18:19:38 -0400

    Updated climate notebook with precipiation graph.

---
+++ app.py
@@ -0,0 +1,26 @@
+#SQL Alchemy HW for Diana Kennen
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+hello_dict = {"Hello": "World!"}
+
+
+@app.route("/")
+def home():
+    return "Hi"
+
+
+@app.route("/normal")
+def normal():
+    return hello_dict
+
+
+@app.route("/jsonified")
+def jsonified():
+    return jsonify(hello_dict)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
