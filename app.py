3eb3e83375a2c6b66ff10fe81e0d4227b98894cc
dlkennen <dlkennen@comcast.net>
2021-05-21 16:17:43 -0400

    Corrected dynamic flask app

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 #SQL Alchemy HW for Diana Kennen
+
+#Set up dependencies
 import numpy as np
+import pandas as pd
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, and_
 
 from flask import Flask, jsonify
 
@@ -23,8 +26,6 @@ Measurement = Base.classes.measurement
 #Setting Up Flask
 app = Flask(__name__)
 
-
-
 #Setting up Flask Routes for homepage
 
 @app.route("/")
@@ -35,11 +36,13 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/temp/<start><br/>"
+        f"/api/v1.0/temp/<start>/<end>"
     )
 
-@app.route("/precipitation")
+#Precipitation Route
+
+@app.route("/api/v1.0/precipitation")
 def precipitation():
     #Create session link
     session = Session(engine)
@@ -51,6 +54,7 @@ def precipitation():
 
     session.close()
 
+    #Create dictionary of dates and precipitation
     all_precip = []
     for prcp, date in precip:
         precip_dict = {}
@@ -59,7 +63,9 @@ def precipitation():
         all_precip.append(precip_dict)
     return jsonify(all_precip)
 
-@app.route("/stations")
+#Station Route
+
+@app.route("/api/v1.0/stations")
 def stations():
     #Create session link
     session = Session(engine)
@@ -74,29 +80,33 @@ def stations():
     
     return jsonify(station_list)
 
-@app.route("/tobs")
+#Temperature Route
+
+@app.route("/api/v1.0/tobs")
 def tobs():
     #Create session link
     session = Session(engine)
 
     #Query list of stations
-    temps = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == 'USC00519281')
+    temps = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281')
 
     session.close()
 
+    #Create list of temperatures
     temp_list = []
-    for date, tobs in temps:
-        temp_dict = {}
-        temp_dict["date"] = date
-        temp_dict["tobs"] = tobs
-        temp_list.append(temp_dict)
+    for t in temps:
+        if type(t.tobs) == float:
+            temp_list.append(t.tobs)
+
     return jsonify(temp_list)
 
-@app.route("/<start>")
+#Start Date Route
+
+@app.route("/api/v1.0/temp/<start>")
 def start(start):
-
-    start_date = str(start)
-
+    
+    start_date = start
+    
     session = Session(engine)
 
     # Using the most active station id and start date, calculate the lowest, highest, and average temperature.
@@ -104,19 +114,19 @@ def start(start):
         filter(Measurement.station == 'USC00519281').\
         filter(Measurement.date >= start_date)
     
-    Session.close()
-
-    #Store minimum temperature
-    minT = min(station_pick)
+    session.close()
     
-    #Store maximum temperature
-    maxT = max(station_pick)
-
     #Eliminating missing data
     temp_list = []
     for t in station_pick:
         if type(t.tobs) == float:
             temp_list.append(t.tobs)
+    
+    #Store minimum temperature
+    minT = min(temp_list)
+    
+    #Store maximum temperature
+    maxT = max(temp_list)
 
     #Importing mean function
     from statistics import mean
@@ -125,36 +135,38 @@ def start(start):
     meanT = round(mean(temp_list), 2)     
 
     #Creating a dictionary of these values
-    Dict = {'Start Date': start, 'Min Temp °F': minT, 'Max Temp °F': maxT, 'Mean Temp °F': meanT}
+    Dict = {'Start Date': start_date, 'Min Temp deg F': minT, 'Max Temp deg F': maxT, 'Mean Temp deg F': meanT}
 
     return jsonify(Dict)
 
-@app.route("/<start>/<end>")
-def start_end(start, end):
-    start_date = str(start)
-    end_date = str(end)
+#Start and End Date Route
 
+@app.route("/api/v1.0/temp/<start>/<end>")
+def end(start):
+    start_date = start
+    end_date = end
+    
     session = Session(engine)
 
-    # Using the most active station id and start date, calculate the lowest, highest, and average temperature.
+     # Using the most active station id and start date, calculate the lowest, highest, and average temperature.
     station_pick = session.query(Measurement.tobs).\
         filter(Measurement.station == 'USC00519281').\
         filter(Measurement.date >= start_date).\
         filter(Measurement.date <= end_date)
     
-    Session.close()
-
-    #Store minimum temperature
-    minT = min(station_pick)
+    session.close()
     
-    #Store maximum temperature
-    maxT = max(station_pick)
-
     #Eliminating missing data
     temp_list = []
     for t in station_pick:
         if type(t.tobs) == float:
             temp_list.append(t.tobs)
+    
+    #Store minimum temperature
+    minT = min(temp_list)
+    
+    #Store maximum temperature
+    maxT = max(temp_list)
 
     #Importing mean function
     from statistics import mean
@@ -163,9 +175,9 @@ def start_end(start, end):
     meanT = round(mean(temp_list), 2)     
 
     #Creating a dictionary of these values
-    Dict = {'Start Date': start, 'Min Temp °F': minT, 'Max Temp °F': maxT, 'Mean Temp °F': meanT}
-
-    return jsonify(Dict) 
+    Dict = {'Start Date': start_date, 'End Date': end_date, 'Min Temp deg F': minT, 'Max Temp deg F': maxT, 'Mean Temp deg F': meanT}
+    
+    return jsonify(Dict)
 
 if __name__ == "__main__":
     app.run(debug=True)
